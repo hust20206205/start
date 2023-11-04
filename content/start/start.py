b871a0ee7fc687f18e19cb1ee5942f8fa1caed38
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-04 23:10:43 +0700

    2023_11_04_23_10_43_127742

--- content/start/start.py
+++ content/start/start.py
@@ -1,3 +1,20 @@
-print(1)
-from time import sleep
-sleep(10)
+import shutil
+from tkinter import filedialog
+
+
+thu_muc1 = filedialog.askdirectory(
+    initialdir="C:/Users/vvn20206205/Desktop/nghia/src")
+print("🐍 File: start/start.py | Line: 7 | undefined ~ thu_muc1", thu_muc1)
+
+
+thu_muc2 = filedialog.askdirectory(
+    initialdir="C:/Users/vvn20206205/Desktop/start/content")
+print("🐍 File: start/start.py | Line: 10 | undefined ~ thu_muc2", thu_muc2)
+
+
+shutil.rmtree(thu_muc1)
+print(f"Đã sao chép thư mục {thu_muc2} vào {thu_muc1}")
+
+
+shutil.copytree(thu_muc2, thu_muc1)
+print(f"Đã sao chép thư mục {thu_muc2} vào {thu_muc1}")
