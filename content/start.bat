245d4324421df7ce3af63ad73065648c0ebcd40f
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-04 22:35:30 +0700

    2023_11_04_22_35_29_923825

--- content/start.bat
+++ content/start.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 pause
-set "sourcePath=C:\Users\vvn20206205\Desktop\BanDau"
+set "sourcePath=C:\Users\vvn20206205\Desktop\start\content\BanDauSwagger"
 set "destinationPath=C:\Users\vvn20206205\Desktop\nghia\src"
 del /q "%destinationPath%\*.\*"
 for /d %%i in ("%destinationPath%\*") do (
@@ -11,3 +11,4 @@ xcopy /s /i /y "%sourcePath%\*" "%destinationPath%"
 cd /d "C:\Users\vvn20206205\Desktop\nghia\workspace"
 code .\vvn20206205.code-workspace
 endlocal
+
