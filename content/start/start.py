2cb4ab143a03b09637078a3210cfea50002072d6
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-04 23:12:23 +0700

    2023_11_04_23_12_23_006851

--- content/start/start.py
+++ content/start/start.py
@@ -2,23 +2,23 @@ import shutil
 from tkinter import filedialog
 
 
-thu_muc1 = filedialog.askdirectory(
+folder1 = filedialog.askdirectory(
     initialdir="C:/Users/vvn20206205/Desktop/nghia/src",
     title="Select source directory"
 )
-print(f"Selected source directory: {thu_muc1}")
+print(f"Selected source directory: {folder1}")
 
 
-thu_muc2 = filedialog.askdirectory(
+folder2 = filedialog.askdirectory(
     initialdir="C:/Users/vvn20206205/Desktop/start/content",
     title="Select destination directory"
 )
-print(f"Selected destination directory: {thu_muc2}")
+print(f"Selected destination directory: {folder2}")
 
 
-shutil.rmtree(thu_muc1)
-print(f"Removed directory: {thu_muc1}")
+shutil.rmtree(folder1)
+print(f"Removed directory: {folder1}")
 
 
-shutil.copytree(thu_muc2, thu_muc1)
-print(f"Copied directory from {thu_muc2} to {thu_muc1}")
+shutil.copytree(folder2, folder1)
+print(f"Copied directory from {folder2} to {folder1}")
